3904e9610e9c34ced9d12f67ce9f139fc8a979f9
monirulislam9549 <monirulislam9549@gmail.com>
2022-05-08 11:19:56 +0600

    footrt modifird

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     const today = new Date()
     const year = today.getFullYear()
     return (
-        <footer className='mt-5 text-center'>
+        <footer className='mt-5 text-center footer'>
             <p> <small> copyright &copy; {year}</small></p>
         </footer>
     );
